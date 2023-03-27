8223aab2c080af0ad649d13d7ff4f06302b92b55
HansolWorld <hs9269@gmail.com>
2023-03-27 22:28:45 +0900

    init project

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by apple on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
